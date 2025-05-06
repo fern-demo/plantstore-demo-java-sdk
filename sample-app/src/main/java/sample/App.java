cb48195d48c549818da30a4450847f76b50036e8
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-06 16:53:43 +0000

    Automated commit message at 2025-05-06T16:53:43.101Z

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -8,6 +8,6 @@ import java.lang.String;
 
 public final class App {
   public static void main(String[] args) {
-    // import com.bloomberg.api.AsyncBloombergApiClient
+    // import com.plantstore.api.AsyncPlantstoreApiClient
   }
 }
